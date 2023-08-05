a75f47ff19050ace949157fb35317fe2d9a55cee
miloDev06 <ytemiliano12@gmail.com>
2023-08-05 16:19:48 -0400

    regla 2 terminada

--- tecnicas.js
+++ tecnicas.js
@@ -1,4 +1,5 @@
 let regla1 = document.querySelector(".section1");
+let regla2 = document.querySelector(".section2")
 let main = document.querySelector(".container");
 
 function verRegla1 (){
@@ -13,5 +14,16 @@ function ocultarRegla1 (){
     regla1.style.display="none";
     main.style.display="flex";
 }
+function verRegla2 (){
+   
+    regla2.style.display="flex";
+    main.style.display="none";
+}
 
-alert("hola")
+
+function ocultarRegla2 (){
+    
+    regla2.style.display="none";
+    main.style.display="flex";
+}
+
